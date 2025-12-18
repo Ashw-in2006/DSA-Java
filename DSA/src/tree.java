2f0e62fd1ef2c569e10d63b248c92190878e95e0
Ashwin <161915771+Ashw-in2006@users.noreply.github.com>
2025-12-18 14:44:47 +0530

    tree prepoio

--- DSA/src/tree.java
+++ DSA/src/tree.java
@@ -1,3 +1,4 @@
+import java.util.*;
 class Node4{
 int data;
 Node4 right;
@@ -10,16 +11,83 @@ Node4(int val){
 }
 class tre{
     Node4 root;
-    public void print(){
-        System.out.println(root.data);
+     int min = Integer.MAX_VALUE;
+    public void ino(Node4 value){
+        if(value==null){
+            return;
+        }
+        if(value.data<min){
+            min = value.data;
+        }
+        ino(value.left);
+        System.out.println(value.data+" ");
+        ino(value.right);
+
+    }
+    public void post(Node4 value){
+        if(value==null){
+            return;
+        }
+        post(value.left);
+        post(value.right);
+        System.out.print("Post :");
+        System.out.print(value.data);
+
+    }
+    int max = 0;
+    public void pre(Node4 value){
+        if(value==null){
+            return;
+        }
+        if(value.data>max){
+            max = value.data;
+            
+        }
+        System.out.println(value.data+" ");
+        
+        pre(value.left);
+        pre(value.right);
         
     }
+    
+    public int preor(Node4 value){
+
+        if(value==null) {
+            return 0;
+        }
+        return 1 + preor(value.left)+preor(value.right);
+    }
+
+    public void leaf(Node4 value){
+        if(value==null){
+            return;
+        }
+        if(value.left==null&&value.right==null){
+            System.out.println(value.data+" ");
+        }
+        leaf(value.left);
+        leaf(value.right);
+        
+
+    }
+
 }
+
 
 public class tree {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         tre  t = new tre();
-        t.root = new Node4(69);
-        t.print();
+        t.root = new Node4(10);
+        t.root.left = new Node4(20);
+        t.root.right = new Node4(30);
+        t.root.left.left = new Node4(40);
+        t.root.left.right = new Node4(50);
+        t.root.right.left = new Node4(60);
+        t.root.right.right = new Node4(70);
+        t.leaf(t.root);
+        // t.pre(t.root);
+        //  System.out.print(t.min);
+        // t.post(t.root);
     }
 }
